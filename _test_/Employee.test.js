82f10f6dd0a48e42393cb897db76433828a5eed7
Donnahue George <Donnahuegjr@gmail.com>
2021-01-21 11:16:10 -0500

    4th test working

--- _test_/Employee.test.js
+++ _test_/Employee.test.js
@@ -1,18 +1,27 @@
 const Employee = require("../Classes/Employee");
 
 describe("Employee class", () => {
-  it("Should display correct name for the Employee", () => {
-    const employee = new Employee("bob", 21, "bob@bob.com");
-    expect(employee.name).toBe("bob");
+  describe("Constructor test", () => {
+    it("Should display correct name for the Employee", () => {
+      const employee = new Employee("bob", 21, "bob@bob.com");
+      expect(employee.name).toBe("bob");
+    });
+
+    it("Should display employee's id number", () => {
+      const employee = new Employee("bob", 21, "bob@bob.com");
+      expect(employee.id).toBe(21);
+    });
+
+    it("Should display employee's email", () => {
+      const employee = new Employee("bob", 21, "bob@bob.com");
+      expect(employee.email).toBe("bob@bob.com");
+    });
   });
 
-  it("Should display employee's id number", () => {
-    const employee = new Employee("bob", 21, "bob@bob.com");
-    expect(employee.id).toBe(21);
-  });
-
-  it("Should display employee's email", () => {
-    const employee = new Employee("bob", 21, "bob@bob.com");
-    expect(employee.email).toBe("bob@bob.com");
+  describe("Function test", () => {
+    it("Should display name when function is called", () => {
+      const employee = new Employee("bob", 21, "bob@bob.com");
+      expect(employee.getName()).toEqual("bob");
+    });
   });
 });
