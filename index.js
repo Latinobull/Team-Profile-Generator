550f18853ab94c79431c7674b591f4081c76c9ed
Donnahue George <Donnahuegjr@gmail.com>
2021-01-21 17:48:47 -0500

    working on starting application working on starting the application

---
+++ index.js
@@ -0,0 +1,5 @@
+const Manager = require("./Classes/Manager");
+const Engineer = require("./Classes/Engineer");
+const Intern = require("./Classes/Intern");
+const inquirer = require("inquirer");
+const fs = require("fs");
