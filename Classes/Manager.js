38c0f2e25f53932147a0db1d866ef3c6860f0802
Donnahue George <Donnahuegjr@gmail.com>
2021-01-21 10:28:27 -0500

    finished manager constructor and functions

--- Classes/Manager.js
+++ Classes/Manager.js
@@ -1,8 +1,15 @@
 const Employee = require("./Employee");
 
-class Manager {
-  constructor(officeNum) {
+class Manager extends Employee {
+  constructor(name, id, email, officeNum) {
     super(name, id, email);
     this.officeNum = officeNum;
+  }
+  getOfficeNum() {
+    return this.officeNum;
+  }
+
+  getRole() {
+    return "Manager";
   }
 }
