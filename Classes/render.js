7e14dd2ad07740757f211385598153ee344a4cf9
Donnahue George <Donnahuegjr@gmail.com>
2021-01-22 12:08:23 -0500

    added function for rendering engineer

--- Classes/render.js
+++ Classes/render.js
@@ -19,3 +19,20 @@ const renderManager = (manager) => {
   dist = replacePlace(dist, "officeNumber", manager.getOfficeNum());
   return template;
 };
+
+const renderManager = (engineer) => {
+  let dist = fs.readFileSync(path.resolve(distdir, "engineer.html"), "utf-8");
+  dist = replacePlace(dist, "name", engineer.getName());
+  dist = replacePlace(dist, "role", engineer.getRole());
+  dist = replacePlace(dist, "email", engineer.getEmail());
+  dist = replacePlace(dist, "id", engineer.getId());
+  dist = replacePlace(dist, "github", engineer.getGithub());
+  return template;
+};
+
+// const render = employees => {
+//     const html = []
+
+// html.push(...employees.filter(emp => emp.getrole()=== "Manager").map(manager ))
+
+// }
