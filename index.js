1983d0ab4701bc02a65458a83dd33959fee039f3
Donnahue George <Donnahuegjr@gmail.com>
2021-01-22 12:32:16 -0500

    adding a const needed for the index file

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const Engineer = require("./Classes/Engineer");
 const Intern = require("./Classes/Intern");
 const inquirer = require("inquirer");
 const fs = require("fs");
+const path = require("path");
+const render = require("./Classes/render");
+
+const OutputDIR = path.resolve(__dirname, "Output");
+const outputPath = path.join(OutputDIR, "TheTeam.html");
