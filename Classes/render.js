98be3690e4905fd44902a184aa200527233c0297
Donnahue George <Donnahuegjr@gmail.com>
2021-01-22 11:41:51 -0500

    adding placeholders through regex

--- Classes/render.js
+++ Classes/render.js
@@ -1,5 +1,11 @@
 const path = require("path");
 const fs = require("fs");
 
-//const pattern = new RegExp("{{ " + placeholder + " }}", "gm");
 //going to need this eventually in this script
+
+const distDir = path.resolve(__dirname, "../dist");
+
+const replacePlace = (dist, placeholder, value) => {
+  const pattern = new RegExp("{{ " + placeholder + " }}", "gm");
+  return replacePlace(pattern, value);
+};
