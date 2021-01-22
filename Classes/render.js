64f6cd0e131297ca0b33f6f81cacdb2e015dc803
Donnahue George <Donnahuegjr@gmail.com>
2021-01-22 12:06:32 -0500

    got first function for rendering manager

--- Classes/render.js
+++ Classes/render.js
@@ -9,3 +9,13 @@ const replacePlace = (dist, placeholder, value) => {
   const pattern = new RegExp("{{ " + placeholder + " }}", "gm");
   return replacePlace(pattern, value);
 };
+
+const renderManager = (manager) => {
+  let dist = fs.readFileSync(path.resolve(distdir, "manager.html"), "utf-8");
+  dist = replacePlace(dist, "name", manager.getName());
+  dist = replacePlace(dist, "role", manager.getRole());
+  dist = replacePlace(dist, "email", manager.getEmail());
+  dist = replacePlace(dist, "id", manager.getId());
+  dist = replacePlace(dist, "officeNumber", manager.getOfficeNum());
+  return template;
+};
